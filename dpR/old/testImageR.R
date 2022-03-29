830952416195cb77450817983c170d2509075271
jidec <>
2022-03-29 14:13:30 -0400

    added skeleton of patternize PCA and moved imageR script

--- dpR/old/testImageR.R
+++ dpR/old/testImageR.R
@@ -4,8 +4,13 @@ install.packages("imager")
 install.packages("ggplot2")
 library(imager)
 library(ggplot2)
-
-img <- load.image("../prelim_experiments/odo_seg_alignment/segs/4155610_198_masked_clust.jpg")
+library(raster)
+img <- load.image("../../experiments/odo_seg_analyzer/images/4155610_198_discrete.png")
+# Use stack function to read in all bands
+RGB_stack_HARV <- stack("../../experiments/odo_seg_analyzer/images/4155610_198_discrete.png")
+# Create an RGB image from the raster stack
+plotRGB(imageList[[1]], 
+        r = 1, g = 2, b = 3)
 plot(img)
 img_df <- as.data.frame(img)
 plot(img_df)
